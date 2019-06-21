0b6b58f586d9b2322118ec635291123667d9deb9
Cássio Paixão <cassio.paixao@gmail.com>
2019-06-21 05:46:29 -0300

    feat: Initial commit

---
+++ precache-manifest.879e244b64cabbe1645c9e9cb7387b37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79caf065424d4b85f4ae7bb2d9c564e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e513956bb45ac497f8",
+    "url": "/static/css/main.5e08d228.chunk.css"
+  },
+  {
+    "revision": "5aff921c38f507e49e7c",
+    "url": "/static/js/2.369f3308.chunk.js"
+  },
+  {
+    "revision": "e7e513956bb45ac497f8",
+    "url": "/static/js/main.ce247249.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "e2a499354e3b1d309ff71584d7290d29",
+    "url": "/static/media/comment.e2a49935.svg"
+  },
+  {
+    "revision": "00e13584091199bddd3785adaf0b9f3d",
+    "url": "/static/media/like.00e13584.svg"
+  },
+  {
+    "revision": "83fd0bcc6cdc21e671ae5edf856ba3ef",
+    "url": "/static/media/logo.83fd0bcc.svg"
+  },
+  {
+    "revision": "22d960a9ac06cc252579791795bdea8c",
+    "url": "/static/media/more.22d960a9.svg"
+  },
+  {
+    "revision": "55e74cfdc1470a74c5bc8c2af6ed9fa0",
+    "url": "/static/media/send.55e74cfd.svg"
+  }
+]);
